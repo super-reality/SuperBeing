787f2b5dc88ca5529071ffae45b0493578d6b14c
alextoti <alexandrostitonis@gmail.com>
2022-02-10 10:28:35 +0200

    changed the openai using axios to the openai api

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { runClients } from "./utilities/runClients.js";
 import { initClassifier, initProfanityClassifier } from "./utilities/textClassifier.js";
 import { error } from './utilities/logger.js';
 import worldManager from "./world/worldManager.js";
+import { init } from "./utilities/makeCompletionRequest.js";
 
 new cors_server(process.env.CORS_PORT, '0.0.0.0');
 
@@ -34,6 +35,7 @@ const db = new database();
 
     new roomManager();
     console.log("agent")
+    init()
 
     const agent = customConfig.instance.get('agent')?.replace('_', ' ');
     defaultAgent = agent;
--- src/utilities/makeCompletionRequest.js
+++ src/utilities/makeCompletionRequest.js
@@ -4,7 +4,14 @@ import { makeModelRequest } from "./makeModelRequest.js";
 import { database } from '../database.js';
 import customConfig from './customConfig.js';
 import { error } from './logger.js';
+import openai from 'openai-api';
+
 config();
+let api;
+
+export async function init() {
+        api = new openai(process.env.OPENAI_API_KEY ?? customConfig.instance.get('openai_api_key'));
+}
 
 export async function makeCompletionRequest(data, speaker, agent, type, engine, log = true) {
         if (customConfig.instance.getBool('use_gptj')) {
@@ -29,27 +36,28 @@ export async function makeCompletionRequest(data, speaker, agent, type, engine, 
 const useDebug = false;
 async function makeOpenAIGPT3Request(data, speaker, agent, type, engine, log = true) {
         if(useDebug) return { success: true, choice: { text: "Default response" } };
-        const API_KEY = process.env.OPENAI_API_KEY ?? customConfig.instance.get('openai_api_key')
-        const headers = {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + API_KEY
-        };
-        try {
-                const gptEngine = engine ?? JSON.parse(((await database.instance.getAgentsConfig('common')).toString())).summarizationModel;
-                const resp = await axios.post(
-                        `https://api.openai.com/v1/engines/${gptEngine}/completions`,
-                        data,
-                        { headers: headers }
-                );
 
-                if (resp.data.choices && resp.data.choices.length > 0) {
-                        let choice = resp.data.choices[0];
+        try {
+                const rr = await api.complete({
+                        engine: engine ?? JSON.parse(((await database.instance.getAgentsConfig('common')).toString())).summarizationModel,
+                        prompt: data['prompt'],
+                        maxTokens: data['max_tokens'],
+                        topP: data['top_p'],
+                        temperature: data['temperature'],
+                        frequencyPenalty: data['frequency_penalty'],
+                        presencePenalty: data['presence_penalty'],
+                        stop: data['stop']
+                })
+
+                if (rr.data.choices && rr.data.choices.length > 0) {
+                        let choice = rr.data.choices[0];
                         return { success: true, choice };
 
                 }
         }
         catch (err) {
                 error(err);
+                error(err.resp);
                 return { success: false };
         }
 }
